66bcf8426303a814d3433b8141fa8535f7f97041
Akshat Gupta <akshatt25g@gmail.com>
2024-01-17 11:53:05 +0530

    commit

--- lib/features/main/widgets/category_icon.dart
+++ lib/features/main/widgets/category_icon.dart
@@ -25,165 +25,185 @@ class _CategoryIconState extends State<CategoryIcon> {
     switch (categoryLower) {
       case 'shopping':
         return Container(
-          width: 60,
-          height: 60,
-          padding: EdgeInsets.all(10),
-          decoration: BoxDecoration(
-            color: const Color.fromARGB(255, 240, 193, 190),
-            borderRadius: BorderRadius.circular(15),
-          ),
-          child: SvgPicture.asset(
-            'assets/images/$categoryLower.svg',
-            width: double.infinity,
-            height: double.infinity,
-            fit: BoxFit.cover,
-          ),
-        );
+            width: 60,
+            height: 60,
+            padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 240, 193, 190),
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: Padding(
+              padding: EdgeInsets.all(5),
+              child: SvgPicture.asset(
+                'assets/images/$categoryLower.svg',
+                width: double.infinity,
+                height: double.infinity,
+                fit: BoxFit.cover,
+              ),
+            ));
       case 'subscriptions':
         return Container(
-          width: 60,
-          height: 60,
-          padding: EdgeInsets.all(10),
-          decoration: BoxDecoration(
-            color: const Color.fromARGB(255, 178, 215, 246),
-            borderRadius: BorderRadius.circular(15),
-          ),
-          child: SvgPicture.asset(
-            'assets/images/$categoryLower.svg',
-            width: double.infinity,
-            height: double.infinity,
-            fit: BoxFit.cover,
-          ),
-        );
+            width: 60,
+            height: 60,
+            padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 178, 215, 246),
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: Padding(
+              padding: EdgeInsets.all(5),
+              child: SvgPicture.asset(
+                'assets/images/$categoryLower.svg',
+                width: double.infinity,
+                height: double.infinity,
+                fit: BoxFit.cover,
+              ),
+            ));
       case 'food':
         return Container(
-          width: 60,
-          height: 60,
-          padding: EdgeInsets.all(10),
-          decoration: BoxDecoration(
-            color: const Color.fromARGB(255, 192, 244, 194),
-            borderRadius: BorderRadius.circular(15),
-          ),
-          child: SvgPicture.asset(
-            'assets/images/$categoryLower.svg',
-            width: double.infinity,
-            height: double.infinity,
-            fit: BoxFit.cover,
-          ),
-        );
+            width: 60,
+            height: 60,
+            padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 192, 244, 194),
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: Padding(
+              padding: EdgeInsets.all(5),
+              child: SvgPicture.asset(
+                'assets/images/$categoryLower.svg',
+                width: double.infinity,
+                height: double.infinity,
+                fit: BoxFit.cover,
+              ),
+            ));
 
       case 'travel':
         return Container(
-          width: 60,
-          height: 60,
-          padding: EdgeInsets.all(10),
-          decoration: BoxDecoration(
-            color: const Color.fromARGB(255, 235, 213, 181),
-            borderRadius: BorderRadius.circular(15),
-          ),
-          child: SvgPicture.asset(
-            'assets/images/$categoryLower.svg',
-            width: double.infinity,
-            height: double.infinity,
-            fit: BoxFit.cover,
-          ),
-        );
+            width: 60,
+            height: 60,
+            padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 235, 213, 181),
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: Padding(
+              padding: EdgeInsets.all(5),
+              child: SvgPicture.asset(
+                'assets/images/$categoryLower.svg',
+                width: double.infinity,
+                height: double.infinity,
+                fit: BoxFit.cover,
+              ),
+            ));
       case 'others':
         return Container(
-          width: 60,
-          height: 60,
-          padding: EdgeInsets.all(10),
-          decoration: BoxDecoration(
-            color: const Color.fromARGB(255, 244, 199, 199),
-            borderRadius: BorderRadius.circular(15),
-          ),
-          child: SvgPicture.asset(
-            'assets/images/$categoryLower.svg',
-            width: double.infinity,
-            height: double.infinity,
-            fit: BoxFit.cover,
-          ),
-        );
+            width: 60,
+            height: 60,
+            padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 244, 199, 199),
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: Padding(
+              padding: EdgeInsets.all(5),
+              child: SvgPicture.asset(
+                'assets/images/$categoryLower.svg',
+                width: double.infinity,
+                height: double.infinity,
+                fit: BoxFit.cover,
+              ),
+            ));
       case 'job salary':
         return Container(
-          width: 60,
-          height: 60,
-          padding: EdgeInsets.all(10),
-          decoration: BoxDecoration(
-            color: Color.fromARGB(255, 240, 235, 186),
-            borderRadius: BorderRadius.circular(15),
-          ),
-          child: SvgPicture.asset(
-            'assets/images/job_salary.svg',
-            width: double.infinity,
-            height: double.infinity,
-            fit: BoxFit.cover,
-          ),
-        );
+            width: 60,
+            height: 60,
+            padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Color.fromARGB(255, 240, 235, 186),
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: Padding(
+              padding: EdgeInsets.all(5),
+              child: SvgPicture.asset(
+                'assets/images/job_salary.svg',
+                width: double.infinity,
+                height: double.infinity,
+                fit: BoxFit.cover,
+              ),
+            ));
       case 'stipend':
         return Container(
-          width: 60,
-          height: 60,
-          padding: EdgeInsets.all(10),
-          decoration: BoxDecoration(
-            color: Color.fromARGB(255, 193, 181, 235),
-            borderRadius: BorderRadius.circular(15),
-          ),
-          child: SvgPicture.asset(
-            'assets/images/stipend.svg',
-            width: double.infinity,
-            height: double.infinity,
-            fit: BoxFit.cover,
-          ),
-        );
+            width: 60,
+            height: 60,
+            padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Color.fromARGB(255, 193, 181, 235),
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: Padding(
+              padding: EdgeInsets.all(5),
+              child: SvgPicture.asset(
+                'assets/images/stipend.svg',
+                width: double.infinity,
+                height: double.infinity,
+                fit: BoxFit.cover,
+              ),
+            ));
       case 'job bonus':
         return Container(
-          width: 60,
-          height: 60,
-          padding: EdgeInsets.all(10),
-          decoration: BoxDecoration(
-            color: Color.fromARGB(255, 240, 185, 228),
-            borderRadius: BorderRadius.circular(15),
-          ),
-          child: SvgPicture.asset(
-            'assets/images/job_bonus.svg',
-            width: double.infinity,
-            height: double.infinity,
-            fit: BoxFit.cover,
-          ),
-        );
+            width: 60,
+            height: 60,
+            padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Color.fromARGB(255, 240, 185, 228),
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: Padding(
+              padding: EdgeInsets.all(5),
+              child: SvgPicture.asset(
+                'assets/images/job_bonus.svg',
+                width: double.infinity,
+                height: double.infinity,
+                fit: BoxFit.cover,
+              ),
+            ));
       case 'freelancing':
         return Container(
-          width: 60,
-          height: 60,
-          padding: EdgeInsets.all(10),
-          decoration: BoxDecoration(
-            color: Color.fromARGB(255, 188, 242, 247),
-            borderRadius: BorderRadius.circular(15),
-          ),
-          child: SvgPicture.asset(
-            'assets/images/freelancing.svg',
-            width: double.infinity,
-            height: double.infinity,
-            fit: BoxFit.cover,
-          ),
-        );
+            width: 60,
+            height: 60,
+            padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Color.fromARGB(255, 188, 242, 247),
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: Padding(
+              padding: EdgeInsets.all(5),
+              child: SvgPicture.asset(
+                'assets/images/freelancing.svg',
+                width: double.infinity,
+                height: double.infinity,
+                fit: BoxFit.cover,
+              ),
+            ));
       default:
         return Container(
-          width: 60,
-          height: 60,
-          padding: EdgeInsets.all(10),
-          decoration: BoxDecoration(
-            color: Color.fromARGB(255, 221, 237, 187),
-            borderRadius: BorderRadius.circular(15),
-          ),
-          child: SvgPicture.asset(
-            'assets/images/$categoryLower.svg',
-            width: double.infinity,
-            height: double.infinity,
-            fit: BoxFit.cover,
-          ),
-        );
+            width: 60,
+            height: 60,
+            padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Color.fromARGB(255, 221, 237, 187),
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: Padding(
+              padding: EdgeInsets.all(5),
+              child: SvgPicture.asset(
+                'assets/images/$categoryLower.svg',
+                width: double.infinity,
+                height: double.infinity,
+                fit: BoxFit.cover,
+              ),
+            ));
     }
   }
 }
